5ec7774d3e379168a5be7d7b799a7c030bf255a8
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-03-04 22:50:04 -0600

    Se mejoro la vista y funcionalidad del js

--- functions.js
+++ functions.js
@@ -9,15 +9,18 @@ const btnFetch1 = document.querySelector('#btnFetch1'),
  * Creamos las funciones para cada evento fetch
  */
 const fetchJson = () => {
+    showLoader('#loader1', '#fetch1')
     fetch('./pokemon.json')
     .then( response => response.json() )
     .then( (data) => {
         const fetch1 = document.querySelector('#fetch1')
         fetch1.setAttribute('src', data.image)
         console.log(data)
+        showLoader('#loader1', '#fetch1')
     })
 }
 const fetchImage = () => {
+    showLoader('#loader2', '#fetch2')
     fetch('bulbasaur.png')
     .then( response => response.blob() ) // blob = binary large objet (Recibimos un objeto binario)
     .then( (binayrLargeObject) => {
@@ -25,16 +28,24 @@ const fetchImage = () => {
         const fetch1 = document.querySelector('#fetch1')
         fetch2.setAttribute('src', domString)
         console.log(domString)
+        showLoader('#loader2', '#fetch2')
     })
 }
 const fetchApi = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/25/')
+    showLoader('#loader3', '#fetch3')
+    fetch('https://pokeapi.co/api/v2/pokemon/4/')
     .then( response => response.json() ) // blob = binary large objet (Recibimos un objeto binario)
     .then( (pokemon) => {
         const fetch3 = document.querySelector('#fetch3')
         fetch3.setAttribute('src', pokemon.sprites.front_default)
-        
+        showLoader('#loader3', '#fetch3')
     })
+    
+}
+
+const showLoader = (id, image) =>{
+    document.querySelector(id).classList.toggle('hidde');
+    document.querySelector(image).classList.toggle('hidde');
 }
 
 /**
@@ -43,61 +54,3 @@ const fetchApi = () => {
 btnFetch1.addEventListener("click", fetchJson)
 btnFetch2.addEventListener("click", fetchImage)
 btnFetch3.addEventListener("click", fetchApi)
-
-        // const $pokemon = document.querySelector('#pokemon')
-        // function renderPokemon(image){
-        //     $pokemon.setAttribute('src', image)
-        // }
-
-        // /*
-        // * Este ejemplo hace una peticion get a un json y 
-        // * actualiza la imagen al encontrar el valor image
-        // */
-        // fetch('./pokemon.json')
-        // .then( response => response.json() )
-        // .then( (data) => {
-        //     debugger;
-        //     console.log(data)
-        //     renderPokemon(data.image)
-            
-        // })
-
-
-        // /*
-        // * Este ejemplo hace una peticion get a una imagen directa
-        // * y actualiza la imagen al encontrar el valor image
-        // */
-        // fetch('bulbasaur.png')
-        // .then( response => response.blob() ) // blob = binary large objet (Recibimos un objeto binario)
-        // .then( (binayrLargeObject) => {
-        //     debugger;
-        //     const domString = URL.createObjectURL(binayrLargeObject)
-        //     console.log(domString)
-        //     renderPokemon(domString)
-            
-        // })
-
-        // /*
-        // * Este ejemplo hace una peticion get a una imagen directa
-        // * y actualiza la imagen al encontrar el valor image
-        // */
-        // fetch('bulbasaur.png')
-        // .then( response => response.blob() ) // blob = binary large objet (Recibimos un objeto binario)
-        // .then( (binayrLargeObject) => {
-        //     debugger;
-        //     const domString = URL.createObjectURL(binayrLargeObject)
-        //     console.log(domString)
-        //     renderPokemon(domString)
-            
-        // })
-        // /*
-        // * Este ejemplo hace una peticion get a una imagen directa
-        // * y actualiza la imagen al encontrar el valor image
-        // */
-        // fetch('https://pokeapi.co/api/v2/pokemon/25/')
-        // .then( response => response.json() ) // blob = binary large objet (Recibimos un objeto binario)
-        // .then( (pokemon) => {
-        //     debugger;
-        //     renderPokemon(pokemon.sprites.front_default)
-            
-        // })
